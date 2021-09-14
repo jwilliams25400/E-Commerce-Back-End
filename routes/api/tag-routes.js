26e7c71f8d53a86bb576fb95d3e7d12a2da94844
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-14 01:12:09 -0400

    completed all routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product ******
   try{
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: {model: Product},
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -32,7 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  // create a new tag***************
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
