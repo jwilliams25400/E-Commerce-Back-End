a6ceef7eda30403e46c3bbb383610f058ac76caa
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-13 22:28:33 -0400

    finished product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNuLL: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNuLL: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: "catergory",
+        key: "id",
+        unique: false
+      }
+    },
+
   },
   {
     sequelize,
