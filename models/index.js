a2ef2d72f136f4c8e61df6e6e6f142785783cec0
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-15 22:02:18 -0400

    completed userRoutes and update title on html page

--- models/index.js
+++ models/index.js
@@ -4,23 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category****
 Product.belongsTo(Category, {
   foreignKey: "catergory_id",
 });
-// Categories have many Products
+// Categories have many Products*****
 Category.hasMany(Product, {
   foreignKey: "product_id",
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag)******
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
   },
 });
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)*******
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products**********
   try {
-    const categoryData = await Tag.findAll({
+    const categoryData = await Category.findAll({
       include: {model: Product},
     });
     res.status(200).json(categoryData);
@@ -39,6 +39,7 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
